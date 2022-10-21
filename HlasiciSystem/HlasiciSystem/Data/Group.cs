0874d487ae67108f9353f5a4548eaebd501b39a8
Oliver Šulc <1001@student.itgymnazium.cz>
2022-10-21 18:15:08 +0200

    added basic tables, no identity tables yet

---
+++ HlasiciSystem/HlasiciSystem/Data/Group.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HlasiciSystem.Data
+{
+    public class Group
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [MaxLength(MetaData.NameLength)]
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+
+        public static class MetaData
+        {
+            public const int NameLength = 255;
+        }
+    }
+}
---
+++ HlasiciSystem/HlasiciSystem/Data/UserGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace HlasiciSystem.Data
+{
+    public class UserGroup
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
