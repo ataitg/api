431e90795eff25f1eced6a9458c80e5249710511
Oliver Sulc <1001@adc.itgymnazium.cz>
2022-11-03 02:09:43 +0100

    added endpoint for creating classes

---
+++ HlasiciSystem/HlasiciSystem/Controllers/ClassController.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Data.APIModels;
+using Data.DbModels;
+using Data.Mapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HlasiciSystem.Controllers
+{
+    [ApiController]
+    [Route("[controler]")]
+    public class ClassController : ControllerBase
+    {
+        private readonly IApplicationMapper mapper;
+        private readonly AppDbContext context;
+        public ClassController(IApplicationMapper mapper, AppDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateClass([FromBody] CreateClass model)
+        {
+            var classs = mapper.ToClass(model);
+            context.Classes.Add(classs);
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
