6cd7dc015b44036271fa9b9e28b36ea56f32893b
Albert Jablonsky <a.b.jablonsky@gmail.com>
2022-11-16 21:50:55 +0100

    Added GroupVm model.

---
+++ HlasiciSystem/Data/VModels/GroupVm.cs
@@ -0,0 +1,16 @@
+﻿using Data.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.VModels
+{
+    public class GroupVm
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
