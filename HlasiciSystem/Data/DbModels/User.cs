6239881e035a4f9d738e5fcd41a56ec0721bc2c3
Albert Jablonsky <a.b.jablonsky@gmail.com>
2022-11-16 23:31:16 +0100

    Added GetUsers endpoint and ToUserVm method.

--- HlasiciSystem/Data/DbModels/User.cs
+++ HlasiciSystem/Data/DbModels/User.cs
@@ -1,4 +1,6 @@
 ﻿using Data.Enum;
+using Data.Mapper;
+using Data.VModels;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,17 @@ namespace Data.DbModels
     public class User : IdentityUser<Guid>
     {
         public UserRoles Role { get; set; }
+    }
+
+    public static class UserExtensions
+    {
+        public static UserVm ToUserVm(this IApplicationMapper mapper, User user)
+        {
+            return new()
+            {
+                Id = user.Id.ToString(),
+                UserName = user.UserName,
+            };
+        }
     }
 }
--- HlasiciSystem/HlasiciSystem/Controllers/UserController.cs
+++ HlasiciSystem/HlasiciSystem/Controllers/UserController.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Mapper;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Identity.Attribute;
+using Data.Enum;
+using Data.VModels;
+using Data.DbModels;
 
 namespace HlasiciSystem.Controllers
 {
     public class UserController : ControllerBase
     {
+        private readonly IApplicationMapper mapper;
+        private readonly AppDbContext context;
 
+        public UserController(IApplicationMapper mapper, AppDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [Authorize]
+        [Role(UserRoles.Teacher)]
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new List<UserVm>();
+            context.Users.ToList()
+                .ForEach(user => users.Add(mapper.ToUserVm(user)));
+
+            return Ok(users);
+        }
     }
 }
