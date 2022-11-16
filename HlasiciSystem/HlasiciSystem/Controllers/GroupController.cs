5ad5bf67851d3dbceaafaf4f257a4170e53b16ef
Albert Jablonsky <a.b.jablonsky@gmail.com>
2022-11-16 21:36:37 +0100

    Added RemoveUsersFromGroup endpoint.

--- HlasiciSystem/HlasiciSystem/Controllers/GroupController.cs
+++ HlasiciSystem/HlasiciSystem/Controllers/GroupController.cs
@@ -164,7 +164,35 @@ namespace HlasiciSystem.Controllers
             return Ok();
         }
 
-        
+        [Authorize]
+        [Role(UserRoles.Teacher)]
+        [HttpDelete("/remove/users/group/{id}")]
+        public IActionResult RemoveUsersFromGroup([FromRoute] string groupId, [FromBody] List<string> userIds)
+        {
+            var group = context.Groups.FirstOrDefault(x => x.Id.ToString() == groupId);
+            if (group == null)
+            {
+                return BadRequest();
+            }
+
+            if (group.TeacherId != User.GetUserId())
+            {
+                return Forbid();
+            }
+
+            userIds.ForEach(userId =>
+            {
+                var userGroup = context.UserGroups.FirstOrDefault(x => x.UserId.ToString() == userId && x.GroupId.ToString() == groupId);
+                if (userGroup != null)
+                {
+                    context.UserGroups.Remove(userGroup);
+                    context.SaveChanges();
+                }
+
+            });
+
+            return Ok();
+        }
 
 
     }
