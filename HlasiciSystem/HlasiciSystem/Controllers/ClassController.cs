11be87ded32fcbafeaed8050e6ac0d0945d8c78a
Oliver Šulc <1001@student.itgymnazium.cz>
2022-11-21 08:15:35 +0100

    added AddUserToClass and RemoveUserFromClass endpoints

--- HlasiciSystem/HlasiciSystem/Controllers/ClassController.cs
+++ HlasiciSystem/HlasiciSystem/Controllers/ClassController.cs
@@ -3,10 +3,13 @@ using Data.APIModels;
 using Data.DbModels;
 using Data.Mapper;
 using Data.VModels;
+using Identity.Attribute;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using Data.Enum;
 
 namespace HlasiciSystem.Controllers
 {
@@ -22,15 +25,19 @@ namespace HlasiciSystem.Controllers
             this.context = context;
         }
 
+        [Authorize]
+        [Role(UserRoles.Teacher)]
         [HttpPost("/create/class")]
         public IActionResult CreateClass([FromBody] CreateClass model)
         {
-            var classs = mapper.ToClass(model);
+            var classs = mapper.ToClass(model); 
             context.Classes.Add(classs);
             context.SaveChanges();
             return Ok();
         }
 
+        [Authorize]
+        [Role(UserRoles.Teacher)]
         [HttpDelete("/delete/class/{classId}")]
         public IActionResult DeleteClass([FromRoute] string classId)
         {
@@ -55,7 +62,9 @@ namespace HlasiciSystem.Controllers
             return Ok(classes);
         }
 
-        [HttpGet("get/users/class/{classId}")]
+        [Authorize]
+        [Role(UserRoles.Teacher)]
+        [HttpGet("/get/class/{classId}/users")]
         public IActionResult GetClassUsers([FromRoute] string classId)
         {
             var classs = context.Classes.FirstOrDefault(x => x.Id.ToString() == classId);
@@ -70,6 +79,56 @@ namespace HlasiciSystem.Controllers
                     users.Add(mapper.ToUserVm(UserClass.User));
                 });
             return Ok(users);
+        }
+
+        [Authorize]
+        [Role(UserRoles.Teacher)]
+        [HttpPost("/add/class/{classId}/users")]
+        public IActionResult AddUsersToClass([FromRoute] string classId, [FromBody] Users model)
+        {
+            var classs = context.Classes.FirstOrDefault(x => x.Id.ToString() == classId);
+            if(classs == null)
+            {
+                return BadRequest();
+            }
+            model.Ids.ForEach(userId =>
+            {
+                var user = context.Users.FirstOrDefault(x => x.Id.ToString() == userId);
+                if(user != null)
+                {
+                    context.UserClasses.Add(new()
+                    {
+                        Id = Guid.NewGuid(),
+                        ClassId = classs.Id,
+                        UserId = user.Id
+                    });
+                    context.SaveChanges();
+                }
+            });
+            return Ok();
+        }
+
+
+        [Authorize]
+        [Role(UserRoles.Teacher)]
+        [HttpPost("/remove/class/{classId}/users")]
+        public IActionResult RemoveUsersFromClass([FromRoute] string classId, [FromBody] Users model)
+        {
+            var classs = context.Classes.FirstOrDefault(x => x.Id.ToString() == classId);
+            if(classs == null)
+            {
+                return BadRequest();
+            }
+            model.Ids.ForEach(userId =>
+            {
+                var userClass = context.UserClasses.FirstOrDefault(x => x.UserId.ToString() == userId && x.ClassId == classs.Id);
+                if(userClass != null)
+                {
+                    context.UserClasses.Remove(userClass);
+                    context.SaveChanges();
+                }
+            });
+            return Ok();
         }
     }
 }
